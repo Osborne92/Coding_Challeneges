aaedeee9e7b15aab51e4a39be6c8927dca7218e6
Dane Christensen <danechristensen92@gmail.com>
2020-10-30 14:34:36 -0700

    three more coding challenges

--- python_challenges.py
+++ python_challenges.py
@@ -33,3 +33,34 @@ class Shuffle:
 class Defanged_IP:
     def defangIPaddr(self, address):
         return address.replace('.', '[.]')
+
+class Pairs:
+    def numIdenticalPairs(self, nums):
+        n = len(nums)
+        pairs = 0
+        for i in range(n - 1): # n-1 is neccesary for i to stop one number behind j iterator  
+            for j in range(i + 1, n): # i + 1 is used to start iterating one number after i
+                if nums[i] == nums[j]:
+                    pairs += 1
+        return pairs
+
+class Jewels:
+    def numJewelsInStones(self, J, S):
+        Count = 0
+        for s in S:
+                if s in J:
+                    Count += 1
+        return Count
+
+class Reduce_to_Zero:
+    def numberOfSteps (self, num):
+        count_of_steps = 0
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+            # number is odd...
+                num -= 1
+            count_of_steps += 1
+        return count_of_steps
+        
